7573775849dde1460e2c94246da5509a19ce8532
Serge Lu <Serge.Lu@amd.com>
2025-07-31 13:11:25 +0800

    Fixed unexpected float lds padding

--- projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
+++ projects/hipblaslt/tensilelite/Tensile/KernelWriterAssembly.py
@@ -9989,7 +9989,7 @@ class KernelWriterAssembly(KernelWriter):
       if kernel["EnableMatrixInstruction"]:
         matrixInstK = kernel["MatrixInstK"]
         if kernel["UnrollMajorLDS%s" % tc]:
-          inc = tP["bpeDS"] * max(self.states.numReadsIterCoalescedA,self.states.numReadsIterCoalescedB)
+          inc = int(tP["bpeDS"] * max(self.states.numReadsIterCoalescedA,self.states.numReadsIterCoalescedB))
           comment = " (bpeDS)"
         inc *= matrixInstK
         if kernel["ProblemType"]["Sparse"]:
